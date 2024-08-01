63558c40dc4c316eb07e740da5e58dc8ce00f875
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-01 20:50:11 -0300

    worked in Home.jsx component and style

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='home'>
+      <h2>Monte a sua contagem regressiva!</h2>
+      <form className='countdown-form'>
+
+        <label>
+            <span>Título:</span>
+            <input type="text" name='title' placeholder='Digite o titulo do evento'/>
+        </label>
+
+        <label>
+            <span>Data do evento:</span>
+            <input type="date" name='date'/>
+        </label>
+
+        <label>
+            <span>Imagem:</span>
+            <input type="text" name='image' placeholder='Insira a URL da imagem'/>
+        </label>
+
+        <label>
+            <span>Cor do tema:</span>
+            <input type="color" name='color'/>
+        </label>
+
+        <input type="submit" value="Enviar" />
+
+      </form>
     </div>
   )
 }
