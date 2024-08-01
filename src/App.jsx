c56ed131365a6cd2c6e89d642f2469d81af9cd73
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-01 20:04:24 -0300

    reworking app.jsx and countdown.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 
-import useCountdown from './hooks/useCountdown';
-
-import Title from './components/Title/Title'
-import Counter from './components/Counter/Counter'
-
 import Festa from "./assets/festa.jpg";
 import './App.css'
 import { Outlet } from 'react-router-dom';
 
 function App() {
 
-  const [day,hour,minute,second] = useCountdown("Aug 24, 2024 09:00:00");
-
   return (
     <div className="App" style={{background: `url(${Festa})`}}>
       <div className="container">
-        <Title title="Contagem regressiva para meu curso de Java fullstack começar!"/>
-        <div className="countdown-container">
-
-          <Counter title="Dias" number={day}/>
-          <Counter title="Horas" number={hour}/>
-          <Counter title="Minutos" number={minute}/>
-          <Counter title="Segundos" number={second}/>
-
-        </div>
+        
         <Outlet/>
       </div>
     </div>
--- src/routes/Countdown/Countdown.jsx
+++ src/routes/Countdown/Countdown.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import Title from "../../components/Title/Title";
+import Counter from "../../components/Counter/Counter";
+
+import useCountdown from '../../hooks/useCountdown';
 
 const Countdown = () => {
+    const [day,hour,minute,second] = useCountdown("Aug 24, 2024 09:00:00");
   return (
-    <div>
-      Countdown
-    </div>
+    <>
+      <Title title="Contagem regressiva para meu curso de Java fullstack começar!"/>
+        <div className="countdown-container">
+
+          <Counter title="Dias" number={day}/>
+          <Counter title="Horas" number={hour}/>
+          <Counter title="Minutos" number={minute}/>
+          <Counter title="Segundos" number={second}/>
+
+        </div>
+    </>
   )
 }
 
