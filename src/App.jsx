42b660b6936edeaf244171a1b029f28f61badf39
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-02 00:00:29 -0300

    added dynamic image

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@
 import Festa from "./assets/festa.jpg";
 import './App.css'
 import { Outlet } from 'react-router-dom';
+import { useContext } from "react";
+import { CountdownContext } from "./context/CountdownContext";
 
 function App() {
+  const {event} = useContext(CountdownContext);
+
+  let eventImage = null;
+
+  if(event) eventImage = event.image;
 
   return (
-    <div className="App" style={{background: `url(${Festa})`}}>
+    <div className="App" style={eventImage 
+      ? ({background: `url(${eventImage})`})
+      : ({background: `url(${Festa})`})}>
       <div className="container">
         
         <Outlet/>
