084ad579f124cbacbaf58cc82016b228e0c71117
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-02 00:09:57 -0300

    added dynamic color

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,8 +1,8 @@
 import "./Title.css";
 
-const Title = ({title}) => {
+const Title = ({title,eventColor}) => {
   return (
-    <h1 className='title'>
+    <h1 className='title' style={{color:eventColor}}>
         {title}
     </h1>
   )
